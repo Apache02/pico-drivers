fe32305925619e9b9520fe15502a6580c5667a2e
Aleksey Priymachuk <gxad02@gmail.com>
2024-02-10 21:19:47 +0200

    feat: st7789 interpolate example

---
+++ examples/st7789_interp/st7789_interp.cpp
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/time.h"
+#include "pico/binary_info.h"
+#include "drivers/led/led.h"
+#include "drivers/st7789/io.h"
+#include "drivers/st7789/St7789.h"
+
+#include <math.h>
+#include "hardware/interp.h"
+#include "raspberry_256x256_rgb565.h"
+
+// board config
+#define LED_PIN PICO_DEFAULT_LED_PIN
+
+#define DISPLAY_SPI_SDA 14
+#define DISPLAY_SPI_SCL 15
+#define DISPLAY_SPI_CS 13
+#define DISPLAY_SPI_DC 12
+#define DISPLAY_SPI_RES 11
+#define DISPLAY_SPI_WIDTH 240
+#define DISPLAY_SPI_HEIGHT 320
+
+// global variables
+LED led(LED_PIN);
+St7789_SPI io(DISPLAY_SPI_SDA, DISPLAY_SPI_SCL, DISPLAY_SPI_CS, DISPLAY_SPI_DC, DISPLAY_SPI_RES);
+St7789 display(&io, DISPLAY_SPI_WIDTH, DISPLAY_SPI_HEIGHT);
+
+
+void startup_test() {
+    printf("%s START\n", __FUNCTION__);
+    led.on();
+    sleep_ms(2000);
+    led.off();
+    sleep_ms(10);
+    printf("%s DONE\n", __FUNCTION__);
+}
+
+int main() {
+    bi_decl(bi_program_description("This is a lib-drivers-example binary."));
+    bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
+
+    stdio_init_all();
+
+    startup_test();
+
+    printf("start %s\n", __FUNCTION__);
+
+    uint16_t buffer[display.width];
+
+#define UNIT_LSB 16
+#define LOG_IMAGE_SIZE 8
+    interp_config lane0_cfg = interp_default_config();
+    interp_config_set_shift(&lane0_cfg, UNIT_LSB - 1); // -1 because 2 bytes per pixel
+    interp_config_set_mask(&lane0_cfg, 1, 1 + (LOG_IMAGE_SIZE - 1));
+    interp_config_set_add_raw(&lane0_cfg, true); // Add full accumulator to base with each POP
+    interp_config lane1_cfg = interp_default_config();
+    interp_config_set_shift(&lane1_cfg, UNIT_LSB - (1 + LOG_IMAGE_SIZE));
+    interp_config_set_mask(&lane1_cfg, 1 + LOG_IMAGE_SIZE, 1 + (2 * LOG_IMAGE_SIZE - 1));
+    interp_config_set_add_raw(&lane1_cfg, true);
+
+    interp_set_config(interp0, 0, &lane0_cfg);
+    interp_set_config(interp0, 1, &lane1_cfg);
+    interp0->base[2] = (uint32_t) raspberry_256x256;
+
+    float theta = 0.f;
+    float theta_max = 2.f * (float) M_PI;
+    float theta_step = theta_max / 360;
+    for (;;) {
+        theta += theta_step;
+        if (theta > theta_max) {
+            theta -= theta_max;
+        }
+        int32_t rotate[4] = {
+                static_cast<int32_t>(cosf(theta) * (1 << UNIT_LSB)),
+                static_cast<int32_t>(-sinf(theta) * (1 << UNIT_LSB)),
+                static_cast<int32_t>(sinf(theta) * (1 << UNIT_LSB)),
+                static_cast<int32_t>(cosf(theta) * (1 << UNIT_LSB)),
+        };
+        interp0->base[0] = rotate[0];
+        interp0->base[1] = rotate[2];
+        for (int y = 0; y < 320; ++y) {
+            interp0->accum[0] = rotate[1] * y;
+            interp0->accum[1] = rotate[3] * y;
+            for (int x = 0; x < display.width; ++x) {
+                buffer[x] = *(uint16_t *) (interp0->pop[2]);
+            }
+            display.draw(0, y, display.width - 1, y, buffer, count_of(buffer));
+        }
+    }
+
+    printf("finish %s\n", __FUNCTION__);
+
+    return 0;
+}
